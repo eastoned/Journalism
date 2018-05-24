226a6132874345690cfc25ad79aeba76284cdd77
Easton <eastonself@gmail.com>
2018-05-24 16:49:11 -0400

    fixed everything except mobile and chrome audio

--- recycle.js
+++ recycle.js
@@ -50,6 +50,8 @@ var deblasioSin = 0;
 var mouse, mouseheld;
 var currentCursor;
 var fontBody, fontTitle;
+var endPos;
+var begPos;
 
 function preload(){
 	types[0] = "Organic";
@@ -144,26 +146,32 @@ function setup(){
 	var canvas = createCanvas(800, 600);
 	canvas.parent('game-holder');
 	timeTilSpawn = 120;
-	currentPhrase = "TIME TO START";
+	currentPhrase = "Let's see whatcha got!";
 
 	bins[0] = new bin(100, 450, types[0]);
 	bins[1] = new bin(250, 450, types[1]);
 	bins[2] = new bin(400, 450, types[2]);
 	bins[3] = new bin(550, 450, types[3]);
 
-	goodPhrases[0] = "Correct Phrase 1 Filler";
-	goodPhrases[1] = "Correct Phrase 2 Filler";
-
-	badPhrases[0] = "Incorrect Phrase 1 Filler";
-	badPhrases[1] = "Incorrect Phrase 2 Filler";
+	goodPhrases[0] = "HMPH!";
+	goodPhrases[1] = "Lucky guess...";
+	goodPhrases[2] = "Show off.";
+	goodPhrases[3] = "You don't have to rub it in.";
+	goodPhrases[4] = "What are you trying to prove...";
+	goodPhrases[5] = "The next one won't be as easy.";
 
 	ouchPhrases[0] = "OW!";
-	ouchPhrases[1] = "STOP THAT!";
+	ouchPhrases[1] = "STOP THAT!!";
+	ouchPhrases[2] = "Shouldn't you be doing something?!?";
+	ouchPhrases[3] = "That trash won't recycle itself!!";
+	ouchPhrases[4] = "I'm sensitive there...";
+	ouchPhrases[5] = "ENOUGH! STOP!";
+	ouchPhrases[6] = "Leave me alone! Clean that trash!";
 
 	deblasioScore = deblasio[0];
 	deblasioGame = deblasio[0];
 
-	items.push(new item("Paper", 0));
+	items.push(new item(types[int(random(4))], 0));
 	rectMode(CENTER);
 	imageMode(CENTER);
 	textAlign(CENTER);
@@ -173,30 +181,64 @@ function setup(){
 	currentCursor = mouse;
 	textSize(30);
 	noStroke();
+	endPos = 0;
+	begPos = 0;
 }
 
 function draw(){
 
 	switch(state){
 		case 0:
+			if(!track1.isPlaying()){
+				track1.play();
+			}
+			
 			fill(255);
 			if(tutorialNum == 0){
 				background(205);
 
-				deblasioSin = 30 * sin(millis()/1000);
-				image(deblasioScore, 105, 220 + deblasioSin, deblasioScore.width/5.5, deblasioScore.height/5.5);
-				image(dBin, 100, 300, dBin.width/5, dBin.height/5);
-				image(dLid, 100, 150 + deblasioSin, dLid.width/5, dLid.height/5);
-				fill(255);
-				rect(400, 290, 200, 40);
+				gameTimer++;
+			
+				image(sky, 400, 300, sky.width/3, sky.height/3);
+				image(skyline, 400, 150, skyline.width/3, skyline.height/3);
+				image(deblasioGame, 200 + begPos, 350 + (200 * sin(gameTimer/25)), deblasioGame.width/3, deblasioGame.height/3);
+				image(dLid, 195 + begPos, 255 + (290 * sin(gameTimer/25)), dLid.width/3, dLid.height/3);
+				image(dBin, 195 + begPos, 360 + (20 * sin(gameTimer/25)), dBin.width/2.8, dBin.height/2.8);
+				image(pavement, 400, 550, pavement.width/3, pavement.height/3);
+				image(wall, 400, 365, wall.width/3, wall.height/3);
+				if(sin(gameTimer/25) >= 0){
+					begPos = random(-150, 650);
+				}
+				if(mouseX >= 300 && mouseX <= 500 && mouseY <= 360 && mouseY >= 320){
+					fill(34, 198, 44, 220);
+				} else {
+					fill(255, 170);
+				}
+				rect(400, 340, 200, 40, 15);
+				noStroke();
 				fill(0);
-				text("Click to start!", 400, 300);
+				textSize(30);
+				textFont(fontBody);
+				text("Play!", 400, 350);
+				textSize(100);
+				fill(255);
+				textFont(fontTitle);
+				text("Let’s clean NYC!", 400, 110 + (10 * sin(gameTimer/40)));
+				textSize(25);
+				//text("Show Mayor De Blasio how it’s done.", 580, 130 + (10 * sin(gameTimer/40)));
+				text("Show Mayor De Blasio how it’s done.", 400, 280 + (7 * sin(gameTimer/-30)));
 			} else if (tutorialNum == 1){
 				background(125);
+				textSize(35);
+				text("Put the trash into the right bin.", 400, 550, 700, 100);
 			} else if (tutorialNum == 2){
 				background(255, 0, 0);
+				textSize(35);
+				text("Keep up and don’t put the waste into the wrong bin or the trash will pile up.", 400, 500, 700, 100);
 			} else if (tutorialNum == 3){
 				background(0, 255, 0);
+				textSize(35);
+				text("Set the High Score and show that you can recycle better than De Blasio’s crew.", 400, 500, 700, 100);
 			}
 
 			if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
@@ -206,9 +248,11 @@ function draw(){
 			break;
 		case 1:
 			noCursor();
-
+			textSize(30);
+			textFont(fontBody);
 			if(!loopTrack){
 				track1.play();
+				deblasioGame = deblasio[0];
 				loopTrack = true;
 			}
 			if(loopTrack && track1.currentTime() >= track1.duration() - 1){
@@ -246,7 +290,6 @@ function draw(){
 			}
 
 			for(var i = items.length - 1; i >= 0; i--){
-				
 
 				if(items[i].held){
 					items[i].lift();
@@ -264,7 +307,6 @@ function draw(){
 							}
 						} else {
 							items[i].correct = false;
-							//
 						}
 					}
 				} else {
@@ -280,10 +322,11 @@ function draw(){
 
 				items[i].display();
 			}
-			fill(255);
-			rect(400, 80, 300, 50);
+			fill(255, 200);
+			rect(550, 110, 450, 120, 30);
 			fill(0);
-			text(currentPhrase, 400, 80);
+			textFont(fontTitle);
+			text(currentPhrase, 550, 110, 450, 115);
 
 			if(deblasioReallyHappy){
 				deblasioGame = deblasio[2];
@@ -307,11 +350,9 @@ function draw(){
 				lastUnit = missedItems;
 			}
 
-
-
 			if(timer >= timeTilSpawn){
 				items.push(new item(types[int(random(4))], num));
-				timer = random(-10, 40);
+				timer = random(-10, 20);
 				num++;
 			}
 
@@ -321,79 +362,152 @@ function draw(){
 
 			score = (rightItems - missedItems) * 10;
 			cutTime--;
-			text(score, 50, 40);
 			image(currentCursor, mouseX, mouseY, mouse.width/6, mouse.height/6);
+			if(keyIsDown(32)){
+				state = 2;
+			}
 			break;
 		case 2:
+			cursor(ARROW);
+			textAlign(CENTER);
+			textSize(20);
+			if(score <= 0){
+				score = 0;
+			}
+			textFont(fontBody);
 
 			timer++;
+			gameTimer++;
 
-			if(timer >= 80 && result <=2){
+			if(timer >= 100 && result <=2){
 				result++;
 				timer = 0;
 			}
 
 			background(255);
-			rectMode(CORNER);
-			text("Your Score: " + int(scoreX), 400, 250);
-			image(deblasioScore, 105, 220 + binLidY, deblasioScore.width/5.5, deblasioScore.height/5.5);
-			image(dBin, 100, 300, dBin.width/5, dBin.height/5);
-			image(dLid, 100, 150 + binLidY, dLid.width/5, dLid.height/5);
-			if(scoreX >= 300){
-				fill(0, 0, 255);
-			} else if(scoreX >= 100){
-				fill(0, 255, 0);
-			} else {
-				fill(255, 0, 0);
+			image(sky, 400, 300, sky.width/3, sky.height/3);
+			image(skyline, 400, 150 + endPos, skyline.width/3, skyline.height/3);
+			image(pavement, 400, 550 + endPos, pavement.width/3, pavement.height/3);
+			image(deblasioScore, 200, 175 + (5 * sin(gameTimer/30)) + endPos + binLidY, deblasioScore.width/3, deblasioScore.height/3);
+			image(dLid, 195, 60 + (9 * sin(gameTimer/30)) + endPos + binLidY, dLid.width/3, dLid.height/3);
+			image(dBin, 195, 345 + endPos, dBin.width/2.8, dBin.height/2.8);
+			image(wall, 400, 365 + endPos, wall.width/3, wall.height/3);
+
+			for(var i = 0; i < pile.length; i++){
+				pile[i].display();
+				pile[i].y = (600 - (i * 20)) + endPos;
 			}
-			rect(200, 280, scoreX, 10);
+
+			for(var i = 0; i < bins.length; i++){
+				bins[i].display();
+				bins[i].y = 450 + endPos;
+			}
+
+			rectMode(CORNER);
+			
+			endPos = lerp(endPos, 300, 0.05);
 
 			if(result == 1){
+				
 				scoreX = lerp(scoreX, score, 0.025);
-				binLidY = lerp(binLidY, 50, 0.05);
+				binLidY = lerp(binLidY, 120, 0.05);
 				deblasioScore = deblasio[0];
 
 			}else if(result == 2){
 				scoreX = score;
-				if(score <= -100){
+				textSize(50);
+				if(score <= 0){
 					deblasioScore = deblasio[2];
-					textScore = "YIKES";
-				} else {
-					if(score <= 0){
-						textScore = "OOOF";
-						deblasioScore = deblasio[1];
-					} else if (score <= 50){
-						textScore = "HMMM";
-						deblasioScore = deblasio[0];
-					} else if (score <= 100){
-						textScore = "GOOD..";
-						deblasioScore = deblasio[3];
-					} else if (score > 100){
-						textScore = "WOW!!!";
-						deblasioScore = deblasio[4];
-					}
+					textScore = "Boo!\nYou are a TRASH TURD.";
+				} else if(score <= 50){
+					textScore = "Better luck next time!\nYou are a LITTER LOSER.";
+					deblasioScore = deblasio[1];
+				} else if (score <= 100){
+					textScore = "So close!\nYou are a CAN COLLECTOR.";
+					deblasioScore = deblasio[0];
+				} else if (score <= 150){
+					textScore = "Congratulations!\nYou are RECYCLING ROYALTY!";
+					deblasioScore = deblasio[3];
+				} else if (score > 200){
+					textScore = "We kneel before you!\nYou are a GARBAGE GOD!";
+					deblasioScore = deblasio[4];
 				}
+				textSize(20);
 				cityScoreX = lerp(cityScoreX, cityScore, 0.025);
+				
 				binLidY = lerp(binLidY, -60, 0.05);
 			}else if(result >= 3){
 				scoreX = score;
 				cityScoreX = cityScore;
 				binLidY = -60;
-				text(textScore, 400, 100);
+				textSize(30);
+				textFont(fontTitle);
+				text(textScore, 300, 250 + (5 * sin(gameTimer/-25)), 500, 200);
+				textFont(fontBody);
+				if(mouseX >= 480 && mouseX <= 620 && mouseY >= 370 && mouseY <= 420){
+					fill(34, 198, 44, 220);
+				} else {
+					fill(255, 170);
+					
+				}
+				rect(480, 370, 140, 50, 15);
+				noStroke();
+				fill(0);
+				textSize(20);
+				text("Try Again?", 550, 400);
 			}
-			fill(255, 0, 0);
-			text("Deblasio's Score: " + int(cityScoreX), 400, 320);
-			rect(200, 340, cityScoreX, 10);
+
+			fill(255);
+			textSize(16);
+			textAlign(LEFT);
+			text("*million tons of garbage\ndisposed properly.", 200, 230);
+			textAlign(CENTER);
+			textSize(25);
+			text("Your Score: " + int(scoreX) + "*", 400, 100);
+			if(scoreX >= 200){
+					fill(41, 253, 55);
+			} else if(scoreX >= 150){
+					fill(200, 253, 52);
+			} else if(scoreX >= 100){
+					fill(253, 126, 36);
+			} else if(scoreX >= 50){
+					fill(252, 67, 34);
+			} else {
+					fill(252, 13, 28);
+			}
+			rect(200, 115, scoreX, 20);
+
+
+			fill(255);
+			text("Mayor De Blasio's Score: " + int(cityScoreX) + "*", 400, 175);
+			if(cityScoreX >= 200){
+					fill(41, 253, 55);
+			} else if(cityScoreX >= 150){
+					fill(200, 253, 52);
+			} else if(cityScoreX >= 100){
+					fill(253, 126, 36);
+			} else if(cityScoreX >= 50){
+					fill(252, 67, 34);
+			} else {
+					fill(252, 13, 28);
+			}
+			rect(200, 190, cityScoreX, 20);
+			fill(0, endPos/2.5);
+			rect(0, 528, 800, 72);
+			textAlign(LEFT);
+			textSize(19);
+			fill(255, endPos);
+			text("Credits:\nGame Design: Easton Self\t\t\t\tIllustrations: Asaki Okamura\t\t\t\tSoundtrack: Sheila Bugal", 25, 560);
 	}
 
 }
 
 function mousePressed(){
 	if(state == 0){
-		if(tutorialNum == 0 && mouseX >= 300 && mouseX <= 500 && mouseY <= 310 && mouseY >= 280){
+		if(tutorialNum == 0 && mouseX >= 300 && mouseX <= 500 && mouseY <= 360 && mouseY >= 320){
 			tutorialNum++;
 		} else if (tutorialNum > 0 && tutorialNum < 3){
-			tutorialNum ++;
+			tutorialNum++;
 		} else if(tutorialNum == 3){
 			state++;
 		}
@@ -409,15 +523,15 @@ function mousePressed(){
 		}
 
 		if(mouseX >= 140 && mouseX <= 240 && mouseY >= 50 && mouseY <= 200){
-			currentPhrase = ouchPhrases[int(random(2))];
+			currentPhrase = ouchPhrases[int(random(7))];
 		}
 		
 	}
 
 	if(state == 2){
-		if(result >= 3){
+		if(result >= 3 && mouseX >= 480 && mouseX <= 620 && mouseY >= 370 && mouseY <= 420){
 			reset();
-			setup();
+			state = 1;
 		}
 	}
 
@@ -433,17 +547,16 @@ function mouseReleased(){
 						if(items[i].category == bins[b].category){
 							items[i].returned = true;
 							rightItems++;
-							currentPhrase = goodPhrases[int(random(2))];
+							currentPhrase = goodPhrases[int(random(6))];
 							bins[b].right = true;
-							speed += .045;
-							timeTilSpawn -=.65;
+							speed += .075;
+							timeTilSpawn -= .65;
 							rightCue.play();
 							deblasioSad = true;
-							
 						} else {
-							timeTilSpawn+=.5;
+							timeTilSpawn += .5;
 							missedItems++;
-							currentPhrase = badPhrases[int(random(2))];
+							currentPhrase = items[i].phrase;
 							bins[b].wrong = true;
 							wrongCue.play();
 							deblasioHappy = true;
@@ -461,14 +574,34 @@ function mouseReleased(){
 }
 
 function touchMoved(){
-	//preventDefault();
+
 }
 
 function reset(){
-	state = 0;
+	for(var i = 0; i < bins.length; i++){
+		bins[i].y = 450;
+	}
 	tutorialNum = 0;
+	timer = 0;
+	gameTimer = 0;
+	score = 0;
+	scoreX = 0;
+	rightItems = 0;
+	missedItems = 0;
+	speed = 1;
 	pile = [];
 	items = [];
+	cityScore = 200;
+	cityScoreX = 0;
+	result = 0;
+	deblasioScore = deblasio[0];
+	deblasioGame = deblasio[0];
+	timeTilSpawn = 120;
+	currentPhrase = "Let's see whatcha got!";
+	rectMode(CENTER);
+	imageMode(CENTER);
+	textAlign(CENTER);
+	endPos = 0;
 }
 
 class item{
@@ -478,12 +611,80 @@ class item{
 		this.category = type;
 		if(type == "Organic"){
 			this.img = organicImages[int(random(6))];
+			switch(this.img){
+				case organicImages[0]:
+					this.phrase = "Big Apple into the little BROWN bin, you nut!";
+					break;
+				case organicImages[1]:
+					this.phrase = "Leaves anywhere else than the BROWN bin? I don’t think so.";
+					break;
+				case organicImages[2]:
+					this.phrase = "There’s coffee on this napkin! Put it in the BROWN bin.";
+					break;
+				case organicImages[3]:
+					this.phrase = "Not from NYC, eh? Pizza belongs in the BROWN bin!";
+					break;
+				case organicImages[4]:
+					this.phrase = "This paper plate is soiled! Off to the ORGANIC bin with it!";
+					break;
+				case organicImages[5]:
+					this.phrase = "Who drinks tea?! Well, put it in the ORGANIC bin.";
+					break;
+			}
 		} else if(type =="Paper"){
 			this.img = paperImages[int(random(3))];
+			switch(this.img){
+				case paperImages[0]:
+					this.phrase = "Stay in school and eat your cornflakes. This goes in the PAPER bin!";
+					break;
+				case paperImages[1]:
+					this.phrase = "The egg carton belongs in the PAPER bin, bozo!";
+					break;
+				case paperImages[2]:
+					this.phrase = "It’s a PAPER box! FFS!";
+					break;
+			}
 		} else if(type == "Plastic"){
 			this.img = plasticImages[int(random(5))];
+			switch(this.img){
+				case plasticImages[0]:
+					this.phrase = "Clink it right into the BLUE bin.";
+					break;
+				case plasticImages[1]:
+					this.phrase = "What do you think milk cartons are made of? Not milk...PLASTIC!";
+					break;
+				case plasticImages[2]:
+					this.phrase = "Are you huffing paint? This belongs in the METAL bin.";
+					break;
+				case plasticImages[3]:
+					this.phrase = "You have had your lunch, fatty, now at least put the container in the PLASTIC bin!";
+					break;
+				case plasticImages[4]:
+					this.phrase = "...might is well put it in your bathtub. It goes in the METAL bin.";
+					break;
+			}
 		} else if (type == "Trash"){
 			this.img = trashImages[int(random(6))];
+			switch(this.img){
+				case trashImages[0]:
+					this.phrase = "Are you crazy? Batteries belong in the LANDFILL!";
+					break;
+				case trashImages[1]:
+					this.phrase = "Your diapers belong in the LANDFILL, babyface!";
+					break;
+				case trashImages[2]:
+					this.phrase = "Don’t be ridiculous. Plastic bags go into the LANDFILL!";
+					break;
+				case trashImages[3]:
+					this.phrase = "Poopy di scoop. Scoopy di LANDFILL.";
+					break;
+				case trashImages[4]:
+					this.phrase = "Hey! Wake up! Coffee cups go in the LANDFILL.";
+					break;
+				case trashImages[5]:
+					this.phrase = "With your styrofoam brain, you belong in the LANDFILL, too.";
+					break;
+			}
 		}
 		
 		this.held = false;
